61d8ef782632f32fdd1063e2905a81c65ece6fe2
Georgi-Luckanov <et.georgi.l@eduiteh.eu>
2025-09-18 11:20:56 +0300

    Initial commit

---
+++ temp/temp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("input a number:");
+string input = Console.ReadLine();
+if (!string.IsNullOrEmpty(input))
+{
+    float celsius = float.Parse(input);
+    float kelvin = celsius + 273.15f;
+    Console.WriteLine(kelvin);
+}
