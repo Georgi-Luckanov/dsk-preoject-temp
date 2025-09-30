f1e183badc3f612fd496b7b88f7f975f810bd810
s <drawwithstasi@gmail.com>
2025-09-30 11:15:09 +0300

    Create numbers1-10.cs
    Numbers 1 - 10

---
+++ temp/temp/numbers1-10.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+int number = 1; 
+while (number <= 200)
+{
+    Console.WriteLine(number);
+    number++;
+}
+
+
+for (int i = 1; i <= 200; i++)
+{
+    Console.WriteLine(i);
+}
